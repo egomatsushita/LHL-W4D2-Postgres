40068f4354429fdd82fec781c433b35515233da2
egomatsushita <eduardo.go.rj@gmail.com>
2017-07-18 22:17:28 +0000

    Implement migrations

---
+++ migrations/20170718211933_create_milestones.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', (table) => {
+      table.increments('id');
+      table.string('description', 255).notNullable();
+      table.date('date_achieved');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('milestones');
+};
